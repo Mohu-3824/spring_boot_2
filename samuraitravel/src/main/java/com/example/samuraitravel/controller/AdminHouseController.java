aa6eb6f15bfea7707a30a0024c04e95294ea03e0
Mohu-3824 <maki.yoshimura.97@gmail.com>
2025-08-01 20:11:36 +0900

    民宿一覧ページの更新

---
+++ samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -0,0 +1,32 @@
+package com.example.samuraitravel.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.service.HouseService;
+
+@Controller
+@RequestMapping("/admin/houses")
+public class AdminHouseController {
+    private final HouseService houseService;
+
+    public AdminHouseController(HouseService houseService) {
+        this.houseService = houseService;
+    }
+
+    @GetMapping
+    public String index(@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable, Model model) {
+        Page<House> housePage = houseService.findAllHouses(pageable);
+
+        model.addAttribute("housePage", housePage);
+
+        return "admin/houses/index";
+    }
+}
